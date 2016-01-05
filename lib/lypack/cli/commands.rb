396691c7327b456d391a6c256359c3bf22ccf0a7
Sharon Rosner <ciconia@gmail.com>
2016-01-05 23:57:30 +0100

    List and invoke system installed version of lilypond
    Add handling of default, current version etc.

--- lib/lypack/cli/commands.rb
+++ lib/lypack/cli/commands.rb
@@ -1,18 +1,71 @@
+# package commands
+
+def lilypond_prefix(info)
+  if info[:current] && info[:default]
+    "=* "
+  elsif info[:current]
+    "=> "
+  elsif info[:default]
+    " * "
+  else
+    "   "
+  end
+end
+
+def lilypond_postfix(info)
+  if info[:system]
+    " (system)"
+  else
+    ""
+  end
+end
+
+def format_lilypond_entry(info)
+  "#{lilypond_prefix(info)}#{info[:version]}#{lilypond_postfix(info)}"
+end
+
+LILYPOND_PREAMBLE = <<EOF
+
+Lilypond versions:
+
+EOF
+
+LILYPOND_LEGEND = <<EOF
+
+# => - current
+# =* - current && default
+#  * - default
+
+EOF
+
 command :list do |c|
   c.syntax =      "list [PATTERN]"
   c.description = "Lists installed versions of packages whose name matches PATTERN"
   c.action do |args, opts|
-    Lypack::Package.list(args.first).each {|p| puts p}
+    pattern = args.first
+    if pattern.nil? || pattern == 'lilypond'
+      STDOUT.puts LILYPOND_PREAMBLE
+      Lypack::Lilypond.list.each {|info| puts format_lilypond_entry(info)}
+      STDOUT.puts LILYPOND_LEGEND
+    else
+      Lypack::Package.list(args.first).each {|p| puts p}
+    end
   end
 end
+
+
 
 command :compile do |c|
   c.syntax = "compile <FILE>"
   c.description = "Resolves package dependencies and invokes lilypond"
   c.option '-c', '--config FILE', 'Set config file'
   c.action do |args, opts|
-    raise "File not specified" if args.empty?
-
-    Lypack::Lilypond.compile(ARGV[1..-1])
+    begin
+      raise "File not specified" if args.empty?
+      Lypack::Lilypond.compile(ARGV[1..-1])
+    rescue => e
+      STDERR.puts e.message
+      exit 1
+    end
   end
 end
--- lib/lypack/lilypond.rb
+++ lib/lypack/lilypond.rb
@@ -3,7 +3,99 @@ module Lypack::Lilypond
     def compile(argv)
       fn = Lypack.wrap(argv.pop)
       argv << fn
-      exec("lilypond #{argv.join(' ')}")
+      
+      invoke(argv)
+      exec("#{current_lilypond} #{argv.join(' ')}")
+    end
+    
+    def invoke(argv)
+      lilypond = detect_use_version_argument(argv) || current_lilypond
+      
+      exec("#{lilypond} #{argv.join(' ')}")
+    end
+    
+    def detect_use_version_argument(argv)
+      nil
+    end
+    
+    def default_lilypond
+      Lypack::Settings['lilypond/default']
+    end
+    
+    def set_default_lilypond(path)
+      Lypack::Settings['lilypond/default'] = path
+    end
+    
+    # The current lilypond path is stored in a temporary file named by the 
+    # session id. Thus we can persist the version selected by the user
+    def current_lilypond
+      settings = get_sid_settings(Process.getsid)
+
+      if !settings[:current]
+        settings[:current] = default_lilypond
+        set_sid_settings(Process.getsid, settings)
+      end
+      
+      settings[:current]
+    end
+    
+    def set_current_lilypond(path)
+      settings = get_sid_settings(Process.getsid)
+      settings[:current] = path
+      set_sid_settings(Process.getsid, settings)
+    end
+    
+    def get_sid_settings(sid)
+      YAML.load(IO.read(sid_settings_filename(Process.getsid))) rescue {}
+    end
+    
+    def set_sid_settings(sid, settings)
+      File.open(sid_settings_filename(Process.getsid), 'w+') do |f|
+        f << YAML.dump(settings)
+      end
+    end
+    
+    def sid_settings_filename(sid)
+      "/tmp/lypack.session.#{sid}.yml"
+    end
+    
+    def list
+      lilyponds = list_system_lilyponds # + list_lypack_lilyponds
+    end
+    
+    def list_system_lilyponds
+      list = `which -a lilypond`
+      list = list.lines.map {|f| f.chomp}.reject do |l|
+        l =~ /^#{Lypack::LYPACK_BIN_DIRECTORY}/
+      end
+      
+      return list if list.empty?
+      
+      default = default_lilypond
+      if default.nil?
+        default = list.first
+        set_default_lilypond(default)
+      end
+      
+      current = current_lilypond
+      
+      list.inject([]) do |m, path|
+        begin
+          resp = `#{path} -v`
+          if resp.lines.first =~ /LilyPond ([0-9\.]+)/i
+            m << {
+              path: path,
+              version: $1,
+              system: true,
+              current: path == current,
+              default: path == default
+            }
+          end
+        rescue
+          # ignore error
+        end
+        m
+      end
     end
   end
 end
