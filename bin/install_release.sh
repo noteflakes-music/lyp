bdde09da2b61facd93c9b5b034d63b7e9a91e68f
Sharon Rosner <ciconia@gmail.com>
2016-01-18 16:32:38 +0100

    Add release installation script
    (Don’t forget to update the release version in this script when
    releasing a new version)

---
+++ bin/install_release.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+
+shopt -s extglob
+set -o errtrace
+set -o errexit
+
+fail() { log "\nERROR: $*\n" ; exit 1 ; }
+
+has() {
+  type "$1" > /dev/null 2>&1
+}
+
+download() {
+  if has "curl"; then
+    curl -L -o $*
+  elif has "wget"; then
+    wget -O $*
+  else
+    fail "Could not find curl or wget"
+  fi
+}
+    
+WORKDIR="/tmp/lyp-release-installer"
+LYP_VERSION="0.1.2"
+URL_BASE="https://github.com/noteflakes/lyp/releases/download/v$LYP_VERSION"
+
+PLATFORM=`uname -sp`
+case $PLATFORM in
+  "Linux x86_64")
+    RELEASE_FILE="lyp-$LYP_VERSION-linux-x86_64"
+    ;;
+  "Linux x86")
+    RELEASE_FILE="lyp-$LYP_VERSION-linux-x86"
+    ;;
+  "Darwin i386")
+    RELEASE_FILE="lyp-$LYP_VERSION-osx"
+    ;;
+  *)
+    fail "Unspported platform $PLATFORM"
+esac
+
+RELEASE_URL="$URL_BASE/$RELEASE_FILE.tar.gz"
+RELEASE_PATH="$WORKDIR/$RELEASE_FILE"
+
+rm -rf $WORKDIR
+mkdir $WORKDIR
+echo "Downloading $RELEASE_URL"
+download "$WORKDIR/release.tar.gz" $RELEASE_URL
+echo "Extracting $WORKDIR/release.tar.gz"
+tar -xzf "$WORKDIR/release.tar.gz" -C $WORKDIR
+
+$RELEASE_PATH/bin/lyp install self
+
+rm -rf $WORKDIR
