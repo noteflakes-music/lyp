871cc45e3c0a07d590bc5c25e10a578182cc3c98
Sharon Rosner <ciconia@gmail.com>
2017-01-15 00:41:55 +0100

    Small refactor of wrapper code

--- lib/lyp/wrapper.rb
+++ lib/lyp/wrapper.rb
@@ -3,14 +3,16 @@ module Lyp
     File.expand_path('templates/deps_wrapper.rb', File.dirname(__FILE__))
   ))
 
+  WRAPPERS_DIR = "#{Lyp::TMP_ROOT}/wrappers"
+
   def self.wrap(fn, opts = {})
     r = Lyp::DependencyResolver.new(fn, opts).resolve_package_dependencies
     # copy current_package_dir option
     r[:current_package_dir] = opts[:current_package_dir]
     r[:opts] = opts
 
-    FileUtils.mkdir_p("#{Lyp::TMP_ROOT}/wrappers")
-    fn = "#{Lyp::TMP_ROOT}/wrappers/#{File.basename(fn)}"
+    FileUtils.mkdir_p(WRAPPERS_DIR)
+    fn = "#{WRAPPERS_DIR}/#{File.basename(fn)}"
 
     File.open(fn, 'w+') {|f| f << WRAPPER_TEMPLATE.render(r)}
 
